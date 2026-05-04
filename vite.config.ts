2f25f3f89e3ade9e60b17664f3f522dc7f7a5d74
mariiaplakhtyna <plaxtinamaria@gmail.com>
2026-05-04 10:47:57 -0400

    fix vite base for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/02-react-cafe/',
+  base: '/',
   plugins: [
     react(),
     babel({
